02de3a3b409c8a8a308ad818dd5088155187f2e6
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2024-03-02 13:38:03 +0100

    Update serverList.php

--- html/components/views/configuration/serverList.php
+++ html/components/views/configuration/serverList.php
@@ -9,7 +9,7 @@
         <input type="hidden" name="id" value="<?= $server->id ?>">
         <button type="submit">
             <h1><?= $server->name ?></h1>
-            <p><?= str_pad('', 30 - strlen($server->name), '.') ?>..<?= str_pad($server->power, 5, '.', STR_PAD_LEFT) ?>..<?= str_pad($server->repairability, 2, '.', STR_PAD_LEFT) ?>..<?= str_pad('-' . $server->bugs, 5, '.', STR_PAD_LEFT) ?></p>
+            <p><?= str_pad('', 30 - mb_strlen($server->name), '.') ?>..<?= str_pad($server->power, 5, '.', STR_PAD_LEFT) ?>..<?= str_pad($server->repairability, 2, '.', STR_PAD_LEFT) ?>..<?= str_pad('-' . $server->bugs, 5, '.', STR_PAD_LEFT) ?></p>
         </button>
     </form>
 </li>
